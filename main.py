d6e40520bba56916aa88f7126330435f762e9f58
Ruben Cuevas <rcuevastech@gmail.com>
2019-07-25 11:34:51 -0400

    Should show website

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class CssiUser(ndb.Model):
   last_name = ndb.StringProperty()
   email = ndb.StringProperty()
   lbs_recycled = ndb.IntegerProperty()
+  items_recycled = ndb.StringProperty()
 
 
 jinja_current_directory = jinja2.Environment(
@@ -19,25 +20,26 @@ jinja_current_directory = jinja2.Environment(
 class LandingPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-
-
-    #Post Method, need to figure out how to link to
-    def post(self):
-        user = users.get_current_user()
-        if not user:
-      # You shouldn't be able to get here without being logged in
-            self.error(500)
-            return
-        cssi_user = CssiUser(
-            first_name=self.request.get('first_name'),
-            last_name=self.request.get('last_name'),
-            id=user.user_id())
-        cssi_user.put()
-        self.response.write('Thanks for signing up, %s!' %
-        cssi_user.first_name)
-
         start_template = jinja_current_directory.get_template("welcomepage.html")
         self.response.write(start_template.render())
+
+    #Post Method, need to figure out how to link to
+    #def post(self):
+    #    user = users.get_current_user()
+        #if not user:
+      # You shouldn't be able to get here without being logged in
+        #    self.error(500)
+        #    return
+    #    cssi_user = CssiUser(
+    #        first_name=self.request.get('first_name'),
+    #        last_name=self.request.get('last_name'),
+    #        id=user.user_id())
+    #    cssi_user.put()
+    #    self.response.write('Thanks for signing up, %s!' %
+    #    cssi_user.first_name)
+
+    #    start_template = jinja_current_directory.get_template("welcomepage.html")
+    #    self.response.write(start_template.render())
 
 app = webapp2.WSGIApplication([
   ('/', LandingPage)
